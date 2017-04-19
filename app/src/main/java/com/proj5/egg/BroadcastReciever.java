dd7cefa9d3c623b45af53d9bd6a57d9682abfd39
PatrickHennis <patrickhennis@gmail.com>
2017-04-18 21:42:04 -0400

    added service methods and egg

--- app/src/main/java/com/proj5/egg/BroadcastReciever.java
+++ app/src/main/java/com/proj5/egg/BroadcastReciever.java
@@ -3,6 +3,7 @@ package com.proj5.egg;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by traceys5 on 4/18/17.
@@ -11,7 +12,7 @@ public class BroadcastReciever extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context, "Egg", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/proj5/egg/EggService.java
+++ app/src/main/java/com/proj5/egg/EggService.java
@@ -3,7 +3,7 @@ package com.proj5.egg;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by traceys5 on 4/18/17.
@@ -12,7 +12,23 @@ public class EggService extends Service {
 
     private int currentEggCount;
 
-    @Nullable
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "In Service", Toast.LENGTH_SHORT).show();
+        doAgain();
+
+        stopSelf();
+        //this was zero in example not sure what ive changed it to but it was yelling at me
+        return Service.START_NOT_STICKY;
+    }
+
+    void doAgain(){
+//        Intent startIntent = new Intent("com.proj5.egg.MYACTION");
+//        PendingIntent startPIntent = PendingIntent.getBroadcast(this, 0, startIntent, 0);
+//        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+//        alarm.set(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime() + + TWO_SECONDS, startPIntent);
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
